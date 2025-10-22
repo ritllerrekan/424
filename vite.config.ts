57266fef44c07af966374271461c6b7565a3bd43
supremekai52 <supremekaikaikaikai@gmail.com>
2025-10-22 10:34:52 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     nodePolyfills({
+      include: ['stream', 'crypto', 'util', 'buffer', 'process', 'events'],
       globals: {
         Buffer: true,
         global: true,
@@ -17,6 +18,7 @@ export default defineConfig({
   ],
   optimizeDeps: {
     exclude: ['lucide-react'],
+    include: ['@web3auth/base', '@web3auth/modal', '@web3auth/ethereum-provider'],
     esbuildOptions: {
       define: {
         global: 'globalThis',
@@ -27,9 +29,11 @@ export default defineConfig({
     alias: {
       stream: 'stream-browserify',
       util: 'util',
+      process: 'process/browser',
     },
   },
   define: {
     'process.env': {},
+    global: 'globalThis',
   },
 });
